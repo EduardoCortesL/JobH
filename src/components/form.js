8001af7f70ea46c90cffea0d548b12a42ee6ff10
Eduardo CL <corteslalo97@gmail.com>
2023-04-11 13:53:33 -0700

    first push basic form, using chatGPT to create this. Pure CSS

---
+++ src/components/form.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+const Form = () => {
+  const [name, setName] = useState('');
+  const [companyName, setCompanyName] = useState('');
+  const [jobDescription, setJobDescription] = useState('');
+  const [resume, setResume] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h2></h2>
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="companyName">Company Name:</label>
+        <input type="text" id="companyName" value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="jobDescription">Job Description:</label>
+        <input type="text" id="jobDescription" value={jobDescription} onChange={(e) => setJobDescription(e.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="resume">Resume:</label>
+        <input type="file" id="resume" onChange={(e) => setResume(e.target.files[0])} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
